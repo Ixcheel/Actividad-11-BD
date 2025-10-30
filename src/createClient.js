e1257c23f9895c7643eef74562a26320b150d850
Ixcheel <ixchel_torres@anahuac.mx>
2025-10-29 20:37:21 -0600

    Update createClient.js

--- src/createClient.js
+++ src/createClient.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase= createClient(
-    "https://cfolwugosxeggwomcsrg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmb2x3dWdvc3hlZ2d3b21jc3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MDg1ODYsImV4cCI6MjA3NzE4NDU4Nn0.IUyPE1TKufHO_UVcMRnvcsaJFdl4WCAxe7Hn8LCYGkw"
-)
+// Tu código lee el NOMBRE de la variable que configuraste en Render
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(
+    supabaseUrl, // Usas el valor de la variable de entorno
+    supabaseAnonKey // Usas el valor de la variable de entorno
+);
